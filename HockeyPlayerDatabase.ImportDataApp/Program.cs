f5e7dbe789af59eba9a043aeda600690d5a07fe0
Mário Keméň <mario.kemen@gmail.com>
2017-11-26 12:12:10 +0100

    Add project files.

---
+++ HockeyPlayerDatabase.ImportDataApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HockeyPlayerDatabase.Interfaces;
+using HockeyPlayerDatabase.Model;
+
+namespace HockeyPlayerDatabase.ImportDataApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HockeyPlayerDatabase");
+
+            Club club = new Club(1, "Prvy klub", "Kukucinova 2, Zilina", "http://url");
+            Console.WriteLine(club.ToString());
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HockeyPlayerDatabase/Model/Club.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HockeyPlayerDatabase.Interfaces;
+
+namespace HockeyPlayerDatabase.Model
+{
+    public class Club : IClub
+    {
+        public Club(int id, string name, string address, string url)
+        {
+            Id = id;
+            Name = name;
+            Address = address;
+            Url = url;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Url { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id}, {Name}, {Address}, {Url}";
+        }
+    }
+}
