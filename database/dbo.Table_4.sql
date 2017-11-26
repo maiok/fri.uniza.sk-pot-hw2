5de89d8b9a625b7f1ec4480eb4877317babfb186
Mário Keméň <mario.kemen@gmail.com>
2017-11-26 15:12:16 +0100

    clean

--- database/dbo.Table_4.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Clubs]
-(
-	[ClubID] INT NOT NULL PRIMARY KEY, 
-    [Name] NVARCHAR(30) NOT NULL, 
-    [Address] NVARCHAR(100) NOT NULL, 
-    [Url] NVARCHAR(50) NOT NULL, 
-    CONSTRAINT [PK_Table] PRIMARY KEY ([ClubID])
-)
